e40e8fb54b36c0b2e041ec392fd4a09e1fb4a2cc
wdaws <bill.d.daws@gmail.com>
2019-07-24 20:04:27 -0400

    refactoring main.py to be a little prettier, starting export stuff

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+import export
+import logging
+import algorithms
+
+logger = logging.getLogger(__name__)
+
 def check_equal(L1, L2):
     return len(L1) == len(L2) and sorted(L1) == sorted(L2)
 
@@ -6,82 +13,47 @@ def compare_to_known(l, known):
     """
     return len(l) == len(known) and sorted(l) == known
 
-def epi_generate_primes(n: int) -> list:
-    if n < 2:
-        return []
-    size = (n - 3) // 2 + 1
-    primes = [2]
-    is_prime = [True] * size
-    for i in range(size):
-        if is_prime[i]:
-            p = i * 2 + 3
-            primes.append(p)
-            for j in range(2 * i ** 2 + 6 * i + 3, size, p):
-                is_prime[j] = False
-
-    return primes
-
-def my_generate_primes(n: int) -> list:
-    primes = []
-    for i in range(2, n + 1):
-        is_prime = True
-        for j in primes:
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(i)
-
-    return primes
 
 def get_known():
     """Will only check first million primes.
-    TODO get more
     """
     with open("data/primes1.txt") as f:
         content = f.readlines()
     content = sorted([int(x.strip()) for x in content])
     return content
 
-def pretty_print(trials):
-    for t in trials:
-        # tedious pretty printing
-        print("\t             test: ", t["test"])
-        print("\t           result: ", t["result"])
-        print("\t              epi: ", " ".join(str(x) for x in t["epi"]))
-        print("\t             mine: ", " ".join(str(y) for y in t["mine"]))
-        print("\t            known: ", " ".join(str(k) for k in t["known"]))
-        print("\t epi_equals_known: ", t["epi_equals_known"])
-        print("\tmine_equals_known: ", t["mine_equals_known"])
-        print()
 
 if __name__ == '__main__':
-    # TODO ugly
-    num_tests = 100
+    results_file = "results/", datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    export.write_header(results_file)
+
+    num_tests = 1000000
+    num_failed = 0
     tests = [i for i in range(num_tests)]
     results = []
     known = get_known()
     for test in tests:
-        epi = epi_generate_primes(test)
-        mine = my_generate_primes(test)
+        print("test #", test)
+        epi = algorithms.epi_generate_primes(test)
+        mine = algorithms.my_generate_primes(test)
         known_slice = [i for i in known if i <= test]
         epi_equals_known = compare_to_known(epi, known_slice)
         mine_equals_known = compare_to_known(mine, known_slice)
+        result = check_equal(epi, mine) and mine_equals_known
         results.append({
             "test": test,
-            "result": check_equal(epi, mine) and mine_equals_known,
+            "result": result,
             "epi": epi,
             "mine": mine,
             "known": known_slice,
             "epi_equals_known": epi_equals_known,
             "mine_equals_known": mine_equals_known
         })
+        if not result:
+            num_failed += 1
 
     print("num tests: ", len(results))
     failed = [r for r in results if not r["result"]]
     num_failed = len(failed)
-    if num_failed > 0:
-        pretty_print(failed)
-
     print("num failed: ", num_failed)
     print("pct failed tests: ", (num_failed / num_tests) * 100)
